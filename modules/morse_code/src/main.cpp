69601ed685c6e080da613bd748c057186eab1c2f
Josue-Adiel <extremeboys14@gmail.com>
2024-11-08 21:05:23 -0500

    Convert morse module code from a .ino to a platform.io project

--- modules/morse_code/src/main.cpp
+++ modules/morse_code/src/main.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <TM1637Display.h>
 #include <morse.h>
 
@@ -15,10 +16,10 @@ const uint8_t allON[] = {0xff, 0xff, 0xff, 0xff};   // Create an array that turn
 const uint8_t allOFF[] = {0x00, 0x00, 0x00, 0x00};  // Create an array that turns all segments OFF
 
 // Input/Output Definitions
-int sensorPin = A0;   // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int buttonPin = 5;
-int sensorValue = 0;  // variable to store the value coming from the sensor
+int freqSelectPin = A0;   // select the input pin for the potentiometer
+int morseLedPin = 13;      // select the pin for the LED
+int buttonPin = 5;    // Select the pin for the "send" button
+int freqSelectValue = 0;  // variable to store the value coming from the sensor
 long signal_frequency;
 
 // Game definitions
@@ -30,11 +31,10 @@ String solution_word;
 int solution_frequency;
 
 // Morse code sender definitions
-float wpm_morse = 5.0;
-LEDMorseSender morse_sender(ledPin, wpm_morse);    // The second parameter is for the WPM of the code
+float wpm_morse = 4.0;
+LEDMorseSender morse_sender(morseLedPin, false, wpm_morse);    // The second parameter is for the WPM of the code
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
   pinMode(STRIKE, OUTPUT);
   pinMode(SOLVED, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
@@ -67,8 +67,8 @@ void loop() {
     Serial.println("**********************");
     Serial.println(solution_word);
     Serial.println(solution_frequency);
-    sensorValue = analogRead(sensorPin);
-    signal_frequency = (((long)sensorValue * 105)/1024) + 3500;
+    freqSelectValue = analogRead(freqSelectPin);
+    signal_frequency = (((long)freqSelectValue * 105)/1024) + 3500;
     signal_frequency = signal_frequency - (signal_frequency % 5);
     display.showNumberDec(signal_frequency);
 
@@ -88,4 +88,3 @@ void loop() {
   digitalWrite(SOLVED, HIGH);
   while(1){}
 }
-
