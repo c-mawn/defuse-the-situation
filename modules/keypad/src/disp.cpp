51556605820b59240c4b8198c4834feeec1bc302
Daniel Theunissen <theundaniel@gmail.com>
2024-11-15 17:14:47 -0500

    Add some sprites and clean up serial debugging

--- modules/keypad/src/disp.cpp
+++ modules/keypad/src/disp.cpp
@@ -10,7 +10,61 @@
 #define OLED_DC       7
 #define OLED_CS       5
 #define OLED_RST      9
+#define BTN1 11
+#define BTN2 12
+#define BTN3 4
+#define BTN4 13
 
+// Button debouncing
+const uint8_t DEBOUNCE_DELAY = 10; // in milliseconds
+
+struct Button {
+    // state variables
+    uint8_t  pin;
+    bool     lastReading;
+    uint32_t lastDebounceTime;
+    uint16_t state;
+
+    // methods determining the logical state of the button
+    bool pressed()                { return state == 1; }
+    bool released()               { return state == 0xffff; }
+    bool held(uint16_t count = 0) { return state > 1 + count && state < 0xffff; }
+
+    // method for reading the physical state of the button
+    void read() {
+        // reads the voltage on the pin connected to the button
+        bool reading = digitalRead(pin);
+
+        // if the logic level has changed since the last reading,
+        // we reset the timer which counts down the necessary time
+        // beyond which we can consider that the bouncing effect
+        // has passed.
+        if (reading != lastReading) {
+            lastDebounceTime = millis();
+        }
+
+        // from the moment we're out of the bouncing phase
+        // the actual status of the button can be determined
+        if (millis() - lastDebounceTime > DEBOUNCE_DELAY) {
+            // don't forget that the read pin is pulled-up
+            bool pressed = reading == LOW;
+            if (pressed) {
+                     if (state  < 0xfffe) state++;
+                else if (state == 0xfffe) state = 2;
+            } else if (state) {
+                state = state == 0xffff ? 0 : 0xffff;
+            }
+        }
+
+        // finally, each new reading is saved
+        lastReading = reading;
+    }
+};
+
+Button button1      = { BTN1, HIGH, 0, 0 };
+Button button2      = { BTN2, HIGH, 0, 0 };
+Button button3      = { BTN3, HIGH, 0, 0 };
+Button button4      = { BTN4, HIGH, 0, 0 };
 
 // Create the OLED display
 Adafruit_SH1106G display = Adafruit_SH1106G(128, 64,OLED_MOSI, OLED_CLK, OLED_DC, OLED_RST, OLED_CS);
@@ -32,47 +86,7 @@ static const unsigned char logo16_glcd_bmp [] PROGMEM = {
 // Store the bitmaps in an array of pointers
 const unsigned char *bitmaps[] = {logo16_glcd_bmp};
 const int numBitmaps = sizeof(bitmaps) / sizeof(bitmaps[0]);
-/*
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  uint8_t icons[NUMFLAKES][3];
 
-  // initialize
-  for (uint8_t f = 0; f < NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-
-    Serial.print("x: ");
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(" y: ");
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(" dy: ");
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  while (1) {
-    // draw each icon
-    for (uint8_t f = 0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SH110X_WHITE);
-    }
-    display.display();
-    delay(200);
-
-    // then erase it + move it
-    for (uint8_t f = 0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SH110X_BLACK);
-      // move it
-      icons[f][YPOS] += icons[f][DELTAY];
-      // if its gone, reinit
-      if (icons[f][YPOS] > display.height()) {
-        icons[f][XPOS] = random(display.width());
-        icons[f][YPOS] = 0;
-        icons[f][DELTAY] = random(5) + 1;
-      }
-    }
-  }
-}
-*/
 void selectRandomElements(int *array, int *selectedElements, int *selectedIndices, int arraySize, int numSelections) {
   for (int i = 0; i < numSelections; i++) {
     int index;
@@ -118,13 +132,30 @@ void getSortedIndices(int *array, int *sortedIndices, int size) {
   }
 }
 
-int *img_order[4];
-int *relative_idx[4];
+int img_order[4];
+int solution[4];
 
 void generateSolution() {
   int randomIndex = random(0, 6);
   int *selectedColumn = columns[randomIndex];
-  selectRandomElements(selectedColumn, *img_order, *relative_idx, 7, 4);
+  Serial.print("Selected Column: ");
+  int column_num = randomIndex + 1;
+  Serial.println(column_num);
+  int relative_idx[4];
+  selectRandomElements(selectedColumn, img_order, relative_idx, 7, 4);
+  Serial.print("Image order: ");
+  for (int i = 0; i<4; i++) {
+    Serial.print(img_order[i]);
+    Serial.print(", ");
+  } 
+  Serial.println("");
+  Serial.print("Relative index: ");
+  for (int i = 0; i<4; i++) {
+    Serial.print(relative_idx[i]);
+    Serial.print(", ");
+  } 
+  Serial.println("");
+  getSortedIndices(relative_idx,solution,4);
 
 }
 
@@ -145,7 +176,7 @@ void initDisplay(int *img_order) {
   // Clear the buffer.
   display.clearDisplay();
   int idx = 0;
-  for (uint8_t f = 0; f < 3; f++) {
+  for (uint8_t f = 0; f < 4; f++) {
       idx = img_order[f];
       display.drawBitmap(X_COORDS[f], Y_COORDS[f], bitmaps[idx], IMG_WIDTH, IMG_HEIGHT, SH110X_WHITE);
     }
@@ -154,19 +185,79 @@ void initDisplay(int *img_order) {
 
 
 }
+int counter = 0;
+void readButtons() {
+  int current;
+  bool pressed = false;
+  button1.read();
+  button2.read();
+  button3.read();
+  button4.read();
+  if (button1.pressed()) {
+    current = 0;
+    pressed = true;
+  }
+  if (button2.pressed()) {
+    current = 1;
+    pressed = true;
+  }
+  if (button3.pressed()) {
+    current = 2;
+    pressed = true;
+  }
+  if (button4.pressed()) {
+    current = 3;
+    pressed = true;
+  }
+
+  if (pressed & (current == solution[counter])) {
+    pressed = false;
+    counter++;
+    Serial.print(counter);
+    if (counter == 4) {
+      digitalWrite(3, HIGH);
+      while(1);
+    }
+  }
+  else if (pressed & (current != solution[counter])) {
+    pressed = false;
+    counter = 0;
+    digitalWrite(2, HIGH);
+    delay(1000);
+    digitalWrite(2, LOW);
+  }
+}
 
 void setup()   {
-  Serial.begin(9600); delay(500);
 
+  pinMode(button1.pin,      INPUT);
+  pinMode(button2.pin,      INPUT);
+  pinMode(button3.pin,      INPUT);
+  pinMode(button4.pin,      INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+
+  Serial.begin(9600); delay(500);
+  
   randomSeed(analogRead(0));  // Seed the random function
   //display.setContrast (0); // dim display
+  for (int i = 0; i<4; i++) {
+    img_order[i] = 0;
+  } 
+  initDisplay(img_order);
+  generateSolution();
 
+  Serial.print("Solution: ");
+  for (int i = 0; i<4; i++) {
+    int button_num = solution[i] + 1;
+    Serial.print(button_num);
+    Serial.print(", ");
+  } 
   
-
   //testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
 }
 
 
 void loop() {
-
+  readButtons();
 }
